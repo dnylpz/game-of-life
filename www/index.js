21e6e0f5bc91e8a8a142f577637857c4ee0f19c3
Daniel Espinoza <jdan_0294@hotmail.com>
2023-12-11 22:18:15 -0500

    fixes some styling adds fps counter
    Signed-off-by: Daniel Espinoza <jdan_0294@hotmail.com>

--- www/index.js
+++ www/index.js
@@ -2,9 +2,10 @@ import  { Universe, Cell } from "game-of-life";
 import  { memory } from "game-of-life/game_of_life_bg.wasm";
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css'
 
 
-const CELL_SIZE = 7;
+const CELL_SIZE = 6;
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
@@ -17,6 +18,50 @@ const playPauseButton = document.getElementById("play-pause");
 const speedControl = document.getElementById("speed");
 const resetButton = document.getElementById("reset");
 const randomizeButton = document.getElementById("randomize");
+
+
+// profiling
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now()
+        const delta =  now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if(this.frames.length > 100 ) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+
+        let mean = sum / this.frames.length;
+
+        this.fps.textContent = `
+Frames per second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+}
+
+
 
 //brushes
 const brushLabel = document.getElementById("brushLabel")
@@ -193,6 +238,7 @@ const drawCells = () => {
 
 
 const renderLoop = () => {
+    fps.render();
     if ( frameCount >= Math.ceil(parseInt(speedControl.value) / 4) ) {
         universe.tick();
         frameCount=1;
